1779d01483631af8b2a304059cdf5ae592d1f711
Aizhan-15 <aizhanurman@gmail.com>
2022-10-20 20:25:43 +0600

    completed all the tasks

--- src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
@@ -2,6 +2,10 @@ package school.mjc.stage0.loops.task3;
 
 public class NinesSum {
     public void calculateSum(int lengthOfLastNumber) {
-
+        int sum;
+        for (sum = 9; lengthOfLastNumber != 0; lengthOfLastNumber = lengthOfLastNumber / 10) {
+            sum = sum + lengthOfLastNumber % 10;
+        }
+        System.out.println(sum);
     }
 }
